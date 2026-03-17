cedcc390142295cc9626a4f8900498e54a3b32f1
Yug-Gupta <yuggupta905@gmail.com>
2026-03-17 16:49:31 +0530

    Create Flask app structure

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
+"""
+app.py
+Entry point for the Flask web application.
+Serves the index.html template containing the embedded Tableau visualisations.
+"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # This will render the index.html file from the 'templates' folder
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # The debug=True flag allows for automatic reloading when you save changes
     app.run(debug=True)
